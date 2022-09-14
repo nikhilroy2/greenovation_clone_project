4a0ab25307921ad74e81eb5cf5d573e8a92461ae
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-09-14 22:39:06 +0600

    homepage working

--- src/jsx/layout/footer/Footer.js
+++ src/jsx/layout/footer/Footer.js
@@ -1,8 +1,128 @@
 import React from 'react';
-
+import '../../../scss/layout/footer/Footer.css';
+import { Link } from 'react-router-dom';
 function Footer(props) {
+    const icon_list = [
+        {
+            id: 1,
+            icon: <svg fill='white' data-bbox="20 20 160 159.999" viewBox="0 0 200 200" height={35} xmlns="http://www.w3.org/2000/svg" data-type="shape">
+                <g>
+                    <path d="M174.754 137.015l-16.878-17.024c-7.055-7.116-18.563-7.186-25.705-.156l-.283.278-.803.794c-5.874 5.808-15.363 5.761-21.178-.105L78.719 89.344c-5.798-5.849-5.75-15.271.108-21.061l.001.001.283-.279c7.142-7.03 7.211-18.499.156-25.615L62.404 25.383c-7.061-7.122-18.581-7.185-25.72-.14l-.001-.001-11.238 11.11c-4.496 4.445-6.458 10.916-4.936 17.042 14.422 58.077 66.325 112.081 124.426 126.073 6.239 1.503 12.817-.21 17.377-4.711l12.003-11.848.284-.279c7.14-7.03 7.21-18.498.155-25.614z" />
+                </g>
+            </svg>,
+            link: '#'
+        },
+        {
+            id: 2,
+            icon: <svg fill='white' data-bbox="20 44.5 160 110.999" viewBox="0 0 200 200" height={28} xmlns="http://www.w3.org/2000/svg" data-type="shape">
+                <g>
+                    <path d="M109.336 104.331a17.481 17.481 0 0 1-18.671 0L20.222 59.784H20v78.442c0 9.54 7.784 17.273 17.386 17.273h125.228c9.602 0 17.386-7.733 17.386-17.273V59.784h-.222l-70.442 44.547z" />
+                    <path d="M22.578 44.5l.215.125 68.173 43.111a16.917 16.917 0 0 0 18.069 0l68.173-43.111.215-.125H22.578z" />
+                </g>
+            </svg>,
+            link: '#'
+        },
+        {
+            id: 3,
+            icon: <svg fill='white' data-bbox="37.5 20 125 160" viewBox="0 0 200 200" height={43} width={43} xmlns="http://www.w3.org/2000/svg" data-type="shape">
+                <g>
+                    <path d="M133.406 122.514c-1.964 3.418-.718 7.826 2.777 9.632 7.917 4.09 12.427 9.062 12.427 13.059 0 8.311-19.389 20.877-48.611 20.877-29.223 0-48.611-12.566-48.611-20.877 0-4.009 4.533-8.996 12.491-13.094 3.507-1.806 4.754-6.229 2.777-9.651a6.95 6.95 0 0 0-9.213-2.693C45.188 126.114 37.5 135.15 37.5 145.206 37.5 164.42 65.481 180 100 180s62.5-15.58 62.5-34.794c0-10.033-7.656-19.053-19.867-25.397a6.95 6.95 0 0 0-9.227 2.705z" />
+                    <path d="M100 20c-24.296 0-43.992 19.737-43.992 44.083a43.895 43.895 0 0 0 4.823 20.022c.11.216.231.427.359.633l32.923 53.004c2.716 4.372 9.057 4.372 11.773 0l32.923-53.004c.128-.206.249-.417.359-.633a43.898 43.898 0 0 0 4.823-20.022C143.992 39.737 124.296 20 100 20zm9.127 57.962c-14.009 9.714-29.286-5.595-19.592-19.632a5.329 5.329 0 0 1 1.338-1.341c14.009-9.717 29.288 5.594 19.593 19.632a5.382 5.382 0 0 1-1.339 1.341z" />
+                </g>
+            </svg>,
+            link: '#'
+        },
+        {
+            id: 4,
+            icon: <img alt="" style={{ width: '45px', height: '45px', objectFit: 'cover' }} src="https://static.wixstatic.com/media/11062b_cef3b719166a4815b446d4dcfcb6120d~mv2.png/v1/fill/w_45,h_45,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_cef3b719166a4815b446d4dcfcb6120d~mv2.png" />,
+            link: '#'
+        },
+        {
+            id: 5,
+            icon: <img alt="" style={{ width: '45px', height: '45px', objectFit: 'cover' }} src="https://static.wixstatic.com/media/11062b_fe985b889c144b348eefc7bbc67018b4~mv2.png/v1/fill/w_45,h_45,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_fe985b889c144b348eefc7bbc67018b4~mv2.png" />,
+            link: '#'
+        },
+    ]
+
+    const action_list = [
+        {
+            id: 1,
+            name: 'Bankverbindung',
+            link: '#'
+        },
+        {
+            id: 2,
+            name: `FAQ's`,
+            link: '#'
+        },
+        {
+            id: 3,
+            name: `Job's`,
+            link: '#'
+        },
+        {
+            id: 4,
+            name: `Geschäftspräsentation`,
+            link: '#'
+        },
+        {
+            id: 5,
+            name: `AGB's`,
+            link: '#'
+        },
+        {
+            id: 6,
+            name: `Datenschutz`,
+            link: '#'
+        },
+        {
+            id: 7,
+            name: `Impressum`,
+            link: '#'
+        },
+    ]
     return (
-        <footer></footer>
+        <footer id='Footer'>
+            <div className="img_logo text-center d-flex justify-content-center">
+                <img src={require('../../../img/logo/greenovation_logo_white.webp')} alt="" />
+
+            </div>
+
+            <ul className="d-flex flex-wrap justify-content-center align-items-center">
+                {
+                    icon_list.map(v => {
+                        return (
+                            <li key={v.id} className="m-3 mx-4">
+                                <Link to={v.link}>
+                                    {v.icon}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <ul className="d-flex flex-wrap justify-content-center align-items-center">
+                {
+                    action_list.map(v => {
+                        return (
+                            <li key={v.id} className="m-3 mx-4">
+                                <Link to={v.link} className="text-white">
+                                    {v.name}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <div className="footer_light_text bg-white p-3 text-center">
+                <p className='mb-0'>
+                    GREENOVATION WERKSTRASSE 3 3428 WILER
+                </p>
+                <p className="font_8 mb-0" style={{ fontSize: '12px', lineHeight: '1.8em', textAlign: 'center' }}><span style={{ fontSize: '12px' }}><span className="color_13"><span style={{ letterSpacing: '0.1em' }}>© 2022 Greenovation GmbH. Website by <a href="http://www.brand-brain.ch" target="_blank" rel="noreferrer noopener"><span style={{ fontWeight: 'bold' }}>BRAND</span>BRAIN</a></span></span></span></p>
+            </div>
+        </footer>
     );
 }
 
--- src/jsx/pages/homepage/Homepage.js
+++ src/jsx/pages/homepage/Homepage.js
@@ -4,13 +4,12 @@ function Homepage(props) {
     return (
         <div id='Homepage'>
             <Section1></Section1>
+            <Section2></Section2>
         </div>
     );
 }
 
 export default Homepage;
-
-
 const Section1 = () => {
     return <section id="Section1">
         <div className="header_placeholder"></div>
@@ -21,13 +20,41 @@ const Section1 = () => {
             <p className="font_7" style={{ lineHeight: '1.8em', textAlign: 'center', fontSize: '24px' }}><span className="color_14"><span style={{ letterSpacing: '0.05em' }}>100% SWISS MADE</span></span></p>
             <div className="content_footer">
                 <div className="wrapper">
-                    <img src={require('../../../img/icons/GreenovationIcon.webp')} alt="" />
-                    <img src={require('../../../img/icons/GreenovationIconSm.webp')} alt="GreenovationIconSm.webp" />
+                    <img className='img_lg' src={require('../../../img/icons/GreenovationIcon.webp')} alt="" />
+                    <img className='img_sm' src={require('../../../img/icons/GreenovationIconSm.webp')} alt="GreenovationIconSm.webp" />
                     <a href='#' className="btn_content_footer">
                         KAUFE JETZT DEINE ERSTE PFLANZE
+                        <svg fill='white' style={{ height: '10px' }} className="ms-2" data-bbox="13.05 2.55 33.878 54.8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
+                            <g>
+                                <path d="M46.5 28.9L20.6 3c-.6-.6-1.6-.6-2.2 0l-4.8 4.8c-.6.6-.6 1.6 0 2.2l19.8 20-19.9 19.9c-.6.6-.6 1.6 0 2.2l4.8 4.8c.6.6 1.6.6 2.2 0l21-21 4.8-4.8c.8-.6.8-1.6.2-2.2z" />
+                            </g>
+                        </svg>
                     </a>
                 </div>
             </div>
+        </div>
+    </section>
+}
+
+const Section2 = () => {
+    return <section id="Section2">
+        <h1 className="font_0" style={{ fontSize: '25px', textAlign: 'center', marginBottom: '55px' }}><span style={{ color: '#85C43F' }}><span style={{ fontSize: '25px' }}>Der am schnellsten wachsende Zukunftsmarkt</span></span></h1>
+
+        <p className="text_content font_0">
+            <span style={{ letterSpacing: '0.05em' }}>Profitiere von den ausserordentlichen Renditen des Cannabismarktes.&nbsp; Wir ermöglichen es Kunden von der ganzen Welt an diesem teilzuhaben - dies ohne die hohen Hürden von Finanzierung, Gesetzgebung und Know-How.</span>
+        </p>
+
+        <h1 className="font_0" style={{ fontSize: '25px', textAlign: 'center' }}><span className="color_23"><span style={{ fontSize: '25px' }}>LEBENSLANG GELD</span></span><span className="color_14"><span style={{ fontSize: '25px' }}>&nbsp;ERHALTEN IN 5 SCHRITTEN</span></span></h1>
+
+        <div className="img_wrapper d-flex justify-content-between">
+            <img src="https://static.wixstatic.com/media/d307ba_a9ab04576f1b48f5bbfc82278fa8f486~mv2.png/v1/fill/w_353,h_433,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/greenovation%20logo%20icon.png" alt="greenovation logo icon.png" style={{ width: '353px', height: '433px', objectFit: 'cover', objectPosition: '50% 50%' }} />
+            <div className="d-flex justify-content-end flex-grow-1">
+                <img className='' src="https://static.wixstatic.com/media/d307ba_766e1eb74e9944d2aa78ba2c83f6eeef~mv2.png/v1/fill/w_324,h_398,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/greenovation%20logo%20icon_edited.png" alt="greenovation logo icon_edited.png" style={{ width: '324px', height: '398px', objectFit: 'cover', objectPosition: '50% 50%' }} />
+            </div>
+            <div className="img_animated">
+                <img src="https://static.wixstatic.com/media/670a64_d7a7f2c910f844fd8049fe77d8da4c02~mv2.jpg/v1/crop/x_646,y_2285,w_3670,h_2531/fill/w_749,h_516,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/GREENOVATION%205%20STEPS.jpg" alt="GREENOVATION 5 STEPS.jpg" style={{ width: '749px', height: '516px', objectFit: 'cover', objectPosition: '50% 50%' }} />
+            </div>
+            <img src="" alt="" />
         </div>
     </section>
 }
