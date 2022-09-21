7796db14da5b2dadf3a0864256086a87861597ab
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-09-21 09:48:57 +0600

    update footer

--- src/jsx/layout/footer/Footer.js
+++ src/jsx/layout/footer/Footer.js
@@ -10,7 +10,8 @@ function Footer(props) {
                     <path d="M174.754 137.015l-16.878-17.024c-7.055-7.116-18.563-7.186-25.705-.156l-.283.278-.803.794c-5.874 5.808-15.363 5.761-21.178-.105L78.719 89.344c-5.798-5.849-5.75-15.271.108-21.061l.001.001.283-.279c7.142-7.03 7.211-18.499.156-25.615L62.404 25.383c-7.061-7.122-18.581-7.185-25.72-.14l-.001-.001-11.238 11.11c-4.496 4.445-6.458 10.916-4.936 17.042 14.422 58.077 66.325 112.081 124.426 126.073 6.239 1.503 12.817-.21 17.377-4.711l12.003-11.848.284-.279c7.14-7.03 7.21-18.498.155-25.614z" />
                 </g>
             </svg>,
-            link: '#'
+            link: '#',
+            
         },
         {
             id: 2,
@@ -48,7 +49,7 @@ function Footer(props) {
         {
             id: 1,
             name: 'Bankverbindung',
-            link: '#'
+            link: '#',
         },
         {
             id: 2,
@@ -88,11 +89,11 @@ function Footer(props) {
 
             </div>
 
-            <ul className="d-flex flex-wrap justify-content-center align-items-center">
+            <ul className="d-flex flex-wrap justify-content-center align-items-center " id='icon_list'>
                 {
                     icon_list.map(v => {
                         return (
-                            <li key={v.id} className="m-3 mx-4">
+                            <li key={v.id} className="m-3 mx-4 mb-0">
                                 <Link to={v.link}>
                                     {v.icon}
                                 </Link>
@@ -106,21 +107,33 @@ function Footer(props) {
                 {
                     action_list.map(v => {
                         return (
-                            <li key={v.id} className="m-3 mx-4">
-                                <Link to={v.link} className="text-white">
-                                    {v.name}
-                                </Link>
+                            <li key={v.id} className="m-3 mx-4 mb-1 pb-4">
+                                {v.id === 3 || v.id === 4 ?
+
+                                    <Link to={v.link} className="text-white" style={{textDecoration: 'underline'}}>
+                                        {v.name}
+                                    </Link>
+
+                                    :
+                                    <Link to={v.link} className="text-white">
+                                        {v.name}
+                                    </Link>
+                                }
+
                             </li>
                         )
                     })
                 }
             </ul>
 
-            <div className="footer_light_text bg-white p-3 text-center">
-                <p className='mb-0'>
+            <div className="footer_light_text bg-white p-3 pb-2 text-center">
+                <p className='my-0'>
                     GREENOVATION WERKSTRASSE 3 3428 WILER
                 </p>
-                <p className="font_8 mb-0" style={{ fontSize: '12px', lineHeight: '1.8em', textAlign: 'center' }}><span style={{ fontSize: '12px' }}><span className="color_13"><span style={{ letterSpacing: '0.1em' }}>© 2022 Greenovation GmbH. Website by <a href="http://www.brand-brain.ch" target="_blank" rel="noreferrer noopener"><span style={{ fontWeight: 'bold' }}>BRAND</span>BRAIN</a></span></span></span></p>
+                <p className="font_8 mb-0" style={{ fontSize: '12px', lineHeight: '1.8em', textAlign: 'center' }}>
+                    <span style={{ fontSize: '12px' }}><span className="color_13"><span style={{ letterSpacing: '0.1em' }}>
+                        © 2022 Greenovation GmbH. Website by <a href="http://www.brand-brain.ch"
+                            target="_blank" rel="noreferrer noopener"><span style={{ fontWeight: 'bold' }}>BRAND</span>BRAIN</a></span></span></span></p>
             </div>
         </footer>
     );
