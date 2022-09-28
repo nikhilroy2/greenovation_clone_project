6121842f040c5c4136e58fc34ca73adf94df51d6
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-09-28 13:53:04 +0600

    update jobs page

--- src/jsx/pages/Jobs/Jobs.js
+++ src/jsx/pages/Jobs/Jobs.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../../../scss/pages/Jobs/Jobs.css';
 function Jobs(props) {
     return (
-        <div>
-            
+        <div id="Jobs">
+            <div className="content_box">
+                <img src={require('../../../img/global/icon_edited.webp')} alt="img" />
+                <p className="font_3" style={{ lineHeight: '1.8em', textAlign: 'center', fontSize: '24px' }}><span style={{ letterSpacing: '0.05em' }}>STELLENANGEBOTE WERDEN BALD AUFGESCHALTET</span></p>
+            </div>
         </div>
     );
 }
